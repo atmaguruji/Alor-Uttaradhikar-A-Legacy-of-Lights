44809e1bdac2985550606e30e8db8471940b753a
atmaguruji <asg.cgc2021.asg05@gmail.com>
2026-07-06 17:59:29 +0530

    Add JavaScript support

---
+++ script.js
@@ -0,0 +1,9 @@
+// Alor Uttaradhikar Website
+
+console.log("Website Loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Welcome to Alor Uttaradhikar");
+
+});
